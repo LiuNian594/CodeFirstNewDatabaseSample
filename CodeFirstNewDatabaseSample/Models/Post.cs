5839a51a753fc02b85abe56e1aab89982363b3e3
baozhenqin <baozhenqin2013@hotmail.com>
2018-12-18 18:16:15 +0800

    EF-Code First

---
+++ CodeFirstNewDatabaseSample/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDatabaseSample.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        //相当于
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+
+    }
+    //多方加入一方键和实体
+}
---
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstNewDatabaseSample.Models;
+using CodeFirstNewDatabaseSample.BusinessLayer;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一个新博客
+            //createBlog();
+            //显示所有博客
+            QueryBlog();
+            Update();
+            QueryBlog();
+            Console.WriteLine("按任意键退出");
+            Console.ReadKey();
+        }
+        static void createBlog()
+        {
+            Console.WriteLine("请输入一个博客名称");
+            string name = Console.ReadLine();
+            Blog blog = new Blog();
+            blog.Name = name;
+            BlogBusinessLayer bbl = new BlogBusinessLayer();
+            bbl.Add(blog);
+        }
+        //显示全部博客
+        static void QueryBlog()
+        {
+            BlogBusinessLayer bbl = new BlogBusinessLayer();
+            var blogs = bbl.Query();
+            foreach(var item in blogs)
+            {
+                Console.WriteLine(item.BlogId + " "+item.Name);
+
+            }
+        }
+        static void Update()
+        {
+            Console.WriteLine("请输入id");
+            int id = int.Parse(Console.ReadLine());
+            BlogBusinessLayer bbl = new BlogBusinessLayer();
+            Blog blog = bbl.Query(id);
+            Console.WriteLine("请输入新名字");
+            string name = Console.ReadLine();
+            blog.Name = name;
+            bbl.Update(blog);
+        }
+    }
+}
